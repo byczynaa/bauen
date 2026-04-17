4abd177947452b2ea7b3a31561c9ea65c605ecfb
byczynaa <112519558+byczynaa@users.noreply.github.com>
2026-04-17 17:38:45 -0400

    second commit, had an issue with the stripe secret key that could obv not be pushed

--- bauen-watches/server.js
+++ bauen-watches/server.js
@@ -2,9 +2,13 @@
 import express from 'express';
 import Stripe from 'stripe';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Charge les variables d'environnement depuis le fichier .env à la racine du projet
+dotenv.config({ path: '../.env' });
 
 const app = express();
-const stripe = new Stripe('STRIPE_SECRET_KEY_REMOVED', { apiVersion: '2023-10-16' }); // Mets ta clé secrète Stripe ici
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2023-10-16' });
 
 app.use(cors());
 app.use(express.json());
